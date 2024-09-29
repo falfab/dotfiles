38769c3d08a6b6ff0e9bafe9d058ae5e87feee18
Fabio Falezza (TV-D-22) <fabio.falezza@bmw.de>
2024-09-29 13:30:36 +0200

    Added GNU Stow

---
+++ nvim/.config/nvim/lua/falfab/options.lua
@@ -0,0 +1,11 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.colorcolumn = "120"
+
+vim.wo.relativenumber = true
+
+vim.opt.termguicolors = true
+
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
---
+++ nvim/.config/nvim/lua/falfab/remap.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+-- vim.keymap.set("n", "<leader>q", vim.cmd.q)
+-- vim.keymap.set("n", "<leader>x", vim.cmd.x)
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>h", vim.cmd.split)
+
+-- Move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Join lines and stay in place
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Move with Ctrl-d and Ctrl-u but stay in place
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in the middle while searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Yank to system register
+vim.keymap.set("n", "Y", "yy")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Remap Esc to Tab and viceversa
+vim.keymap.set("n", "<Tab>", "<Esc>")
+vim.keymap.set("n", "<Tab>", "<Esc>")
+
+
