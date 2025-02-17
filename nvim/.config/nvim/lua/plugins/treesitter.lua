16f757255fc5ac64c0bd2c124f17ce46bf3f7e05
Fabio Falezza <fabio.falezza@gmail.com>
2025-02-17 13:00:49 +0100

    updated nvim plugins list

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.3",
     build = ":TSUpdate",
     config = function()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "cpp", "python", "xml", "typescript", "javascript", "html", "rust", "git_rebase", "gitcommit", "gitignore", "gitattributes" },
+            ensure_installed = { "cpp", "python", "rust", "c", "typescript", "csv", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
+            auto_install = true,
         })
     end
 }
