d96bbd9b8c4b30372a21ef422e6b17c227ec6d59
Fabio Falezza <fabio.falezza@gmail.com>
2025-03-16 20:21:13 +0100

    Fixed formatting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,159 +1,172 @@
 return {
-    {
-        {
-            'williamboman/mason.nvim',
-            lazy = false,
-            opts = {},
-        },
+	{
+		{
+			"williamboman/mason.nvim",
+			lazy = false,
+			opts = {},
+		},
 
-        -- Allow tab autocompletion
-        {
-            "L3MON4D3/LuaSnip",
-            -- follow latest release.
-            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-            -- install jsregexp (optional!).
-            build = "make install_jsregexp"
-        },
+		-- Allow tab autocompletion
+		{
+			"L3MON4D3/LuaSnip",
+			-- follow latest release.
+			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			-- install jsregexp (optional!).
+			build = "make install_jsregexp",
+		},
 
-        -- Autocompletion
-        {
-            'hrsh7th/nvim-cmp',
-            event = 'InsertEnter',
-            config = function()
-                local cmp = require('cmp')
+		-- Autocompletion
+		{
+			"hrsh7th/nvim-cmp",
+			event = "InsertEnter",
+			config = function()
+				local cmp = require("cmp")
 
-                cmp.setup({
-                    sources = {
-                        { name = 'nvim_lsp' },
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                        ['<C-Space>'] = cmp.mapping.complete(),
-                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                        -- Super tab
-                        ['<Tab>'] = cmp.mapping(function(fallback)
-                            local luasnip = require('luasnip')
-                            local col = vim.fn.col('.') - 1
+				cmp.setup({
+					sources = {
+						{ name = "nvim_lsp" },
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-u>"] = cmp.mapping.scroll_docs(-4),
+						["<C-d>"] = cmp.mapping.scroll_docs(4),
+						-- Super tab
+						["<Tab>"] = cmp.mapping(function(fallback)
+							local luasnip = require("luasnip")
+							local col = vim.fn.col(".") - 1
 
-                            if cmp.visible() then
-                                cmp.select_next_item({ behavior = 'select' })
-                            elseif luasnip.expand_or_locally_jumpable() then
-                                luasnip.expand_or_jump()
-                            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-                                fallback()
-                            else
-                                cmp.complete()
-                            end
-                        end, { 'i', 's' }),
+							if cmp.visible() then
+								cmp.select_next_item({ behavior = "select" })
+							elseif luasnip.expand_or_locally_jumpable() then
+								luasnip.expand_or_jump()
+							---@diagnostic disable-next-line: param-type-mismatch
+							elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+								fallback()
+							else
+								cmp.complete()
+							end
+						end, { "i", "s" }),
 
-                        -- Super shift tab
-                        ['<S-Tab>'] = cmp.mapping(function(fallback)
-                            local luasnip = require('luasnip')
+						-- Super shift tab
+						["<S-Tab>"] = cmp.mapping(function(fallback)
+							local luasnip = require("luasnip")
 
-                            if cmp.visible() then
-                                cmp.select_prev_item({ behavior = 'select' })
-                            elseif luasnip.locally_jumpable(-1) then
-                                luasnip.jump(-1)
-                            else
-                                fallback()
-                            end
-                        end, { 'i', 's' }),
-                    }),
-                    snippet = {
-                        expand = function(args)
-                            vim.snippet.expand(args.body)
-                        end,
-                    },
-                })
-            end,
-        },
+							if cmp.visible() then
+								cmp.select_prev_item({ behavior = "select" })
+							elseif luasnip.locally_jumpable(-1) then
+								luasnip.jump(-1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+					}),
+					snippet = {
+						expand = function(args)
+							vim.snippet.expand(args.body)
+						end,
+					},
+				})
+			end,
+		},
 
-        -- LSP
-        {
-            'neovim/nvim-lspconfig',
-            cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-            event = { 'BufReadPre', 'BufNewFile' },
-            dependencies = {
-                { 'hrsh7th/cmp-nvim-lsp' },
-                { 'williamboman/mason.nvim' },
-                { 'williamboman/mason-lspconfig.nvim' },
-                { 'lukas-reineke/lsp-format.nvim' },
-            },
-            init = function()
-                -- Reserve a space in the gutter
-                -- This will avoid an annoying layout shift in the screen
-                vim.opt.signcolumn = 'yes'
-            end,
-            config = function()
-                local lsp_defaults = require('lspconfig').util.default_config
+		{
+			"nvimtools/none-ls.nvim",
+			config = function()
+				local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+				local null_ls = require("null-ls")
+				null_ls.setup({
+					sources = {
+						null_ls.builtins.formatting.stylua,
+						null_ls.builtins.formatting.black,
+					},
+					-- Automatic format
+					on_attach = function(client, bufnr)
+						if client.supports_method("textDocument/formatting") then
+							vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+							vim.api.nvim_create_autocmd("BufWritePre", {
+								group = augroup,
+								buffer = bufnr,
+								callback = function()
+									vim.lsp.buf.format({
+										bufnr = bufnr,
+										filter = function(formatter)
+											return formatter.name == "null-ls"
+										end,
+										asnyc = false,
+									})
+								end,
+							})
+						end
+					end,
+				})
+			end,
+		},
 
-                -- Add cmp_nvim_lsp capabilities settings to lspconfig
-                -- This should be executed before you configure any language server
-                lsp_defaults.capabilities = vim.tbl_deep_extend(
-                    'force',
-                    lsp_defaults.capabilities,
-                    require('cmp_nvim_lsp').default_capabilities()
-                )
+		-- LSP
+		{
+			"neovim/nvim-lspconfig",
+			cmd = { "LspInfo", "LspInstall", "LspStart" },
+			event = { "BufReadPre", "BufNewFile" },
+			dependencies = {
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "williamboman/mason.nvim" },
+				{ "williamboman/mason-lspconfig.nvim" },
+			},
+			init = function()
+				-- Reserve a space in the gutter
+				-- This will avoid an annoying layout shift in the screen
+				vim.opt.signcolumn = "yes"
+			end,
+			config = function()
+				local lsp_defaults = require("lspconfig").util.default_config
 
+				-- Add cmp_nvim_lsp capabilities settings to lspconfig
+				-- This should be executed before you configure any language server
+				lsp_defaults.capabilities = vim.tbl_deep_extend(
+					"force",
+					lsp_defaults.capabilities,
+					require("cmp_nvim_lsp").default_capabilities()
+				)
 
-                -- LspAttach is where you enable features that only work
-                -- if there is a language server active in the file
-                vim.api.nvim_create_autocmd('LspAttach', {
-                    desc = 'LSP actions',
-                    callback = function(event)
-                        local opts = { buffer = event.buf }
+				-- LspAttach is where you enable features that only work
+				-- if there is a language server active in the file
+				vim.api.nvim_create_autocmd("LspAttach", {
+					desc = "LSP actions",
+					callback = function(event)
+						local opts = { buffer = event.buf }
 
-                        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                        vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+						vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+						vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+						vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+						vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+						vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+						vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+						vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+						vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+						vim.keymap.set("n", "<leader>qf", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+						vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
 
-                        local id = vim.tbl_get(event, 'data', 'client_id')
-                        local client = id and vim.lsp.get_client_by_id(id)
-                        if client == nil then
-                            return
-                        end
-                    end,
-                })
+						local id = vim.tbl_get(event, "data", "client_id")
+						local client = id and vim.lsp.get_client_by_id(id)
+						if client == nil then
+							return
+						end
+					end,
+				})
 
-                -- Enable autoformatting on save
-                require("lsp-format").setup {}
-                local on_attach = function(client, bufnr)
-                    require("lsp-format").on_attach(client, bufnr)
-                end
-
-                require('mason-lspconfig').setup({
-                    ensure_installed = { 'lua_ls', 'pyright', 'rust-analyzer' },
-                    handlers = {
-                        -- this first function is the "default handler"
-                        -- it applies to every language server without a "custom handler"
-                        function(server_name)
-                            -- on_attach is used to connect the lsp-format plugin
-                            require('lspconfig')[server_name].setup({ on_attach = on_attach })
-                        end,
-                    }
-                })
-            end
-        }
-    },
-
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require('null-ls')
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.black,
-                },
-            })
-        end
-    }
+				require("mason-lspconfig").setup({
+					ensure_installed = { "lua_ls", "pyright", "rust-analyzer", "clangd" },
+					handlers = {
+						-- this first function is the "default handler"
+						-- it applies to every language server without a "custom handler"
+						function(server_name)
+							-- on_attach is used to connect the lsp-format plugin
+							require("lspconfig")[server_name].setup({})
+						end,
+					},
+				})
+			end,
+		},
+	},
 }
