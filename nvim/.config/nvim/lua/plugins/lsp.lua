93917904a9e32573e397ee0472b892f5fe97d5ac
Fabio Falezza <fabio.falezza@gmail.com>
2025-05-16 23:16:22 +0200

    removed copilot suggestion for chat only experience and improved general quality of life

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
 			"saghen/blink.cmp",
 			-- optional: provides snippets for the snippet source
 			dependencies = {
+				"giuxtaposition/blink-cmp-copilot",
 				"rafamadriz/friendly-snippets",
 				"giuxtaposition/blink-cmp-copilot",
 			},
@@ -40,12 +41,46 @@ return {
 				-- C-k: Toggle signature help (if signature.enabled = true)
 				--
 				-- See :h blink-cmp-config-keymap for defining your own keymap
-				keymap = { preset = "enter" },
+				keymap = { preset = "super-tab" },
 
 				appearance = {
 					-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 					-- Adjusts spacing to ensure icons are aligned
 					nerd_font_variant = "mono",
+					-- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+					kind_icons = {
+						Copilot = "",
+						Text = "󰉿",
+						Method = "󰊕",
+						Function = "󰊕",
+						Constructor = "󰒓",
+
+						Field = "󰜢",
+						Variable = "󰆦",
+						Property = "󰖷",
+
+						Class = "󱡠",
+						Interface = "󱡠",
+						Struct = "󱡠",
+						Module = "󰅩",
+
+						Unit = "󰪚",
+						Value = "󰦨",
+						Enum = "󰦨",
+						EnumMember = "󰦨",
+
+						Keyword = "󰻾",
+						Constant = "󰏿",
+
+						Snippet = "󱄽",
+						Color = "󰏘",
+						File = "󰈔",
+						Reference = "󰬲",
+						Folder = "󰉋",
+						Event = "󱐋",
+						Operator = "󰪚",
+						TypeParameter = "󰬛",
+					},
 				},
 
 				-- (Default) Only show the documentation popup when manually triggered
@@ -55,6 +90,23 @@ return {
 				-- elsewhere in your config, without redefining it, due to `opts_extend`
 				sources = {
 					default = { "lsp", "path", "snippets", "buffer" },
+					providers = {
+						copilot = {
+							name = "copilot",
+							module = "blink-cmp-copilot",
+							score_offset = 100,
+							async = true,
+							transform_items = function(_, items)
+								local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+								local kind_idx = #CompletionItemKind + 1
+								CompletionItemKind[kind_idx] = "Copilot"
+								for _, item in ipairs(items) do
+									item.kind = kind_idx
+								end
+								return items
+							end,
+						},
+					},
 				},
 
 				-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
